ad1d08b16d6a324e8fc44278b0084c2de9598c58
Tigran Aginyan <taginyan91@gmail.comm>
2023-02-12 13:02:35 +0400

    changing code structure and naming

--- script.js
+++ script.js
@@ -1,125 +1,123 @@
-const createPortfolioWebPage = () => {
-	const container = document.querySelector(".container")
-	const todoMiddle = document.querySelector(".todo_middle")
-	const heading = document.querySelector(".heading")
-	const photoDiv = document.querySelector(".photo_div")
-	const introDiv = document.querySelector(".intro_div")
-	const nameElement = document.querySelector(".name")
-	const socialListElements = document.getElementsByTagName("li")
-	const social = document.querySelector(".social")
-	const image = document.querySelector(".image")
-	const portfolio = document.querySelector(".portfolio")
+const p_data = {
+	headingText: "Hi, I'm Software Engineer !",
+}
 
-	const addElementsInHeadingText = (text, titelElement, element) => {
+const createportfolioBlockWebPage = () => {
+	const main = document.querySelector("main")
+	const photoBlock = document.querySelector(".photo_block")
+	const name = document.querySelector(".name")
+	const image = document.querySelector(".image")
+	const introBlock = document.querySelector(".intro_block")
+	const social = document.querySelector(".social")
+	const socialListItems = document.getElementsByTagName("li")
+	const portfolioBlock = document.querySelector(".portfolio_block")
+
+	const addingLettersInHeadingText = (text, headingElem, element) => {
 		for (let i = 0; i < text.length; i++) {
-			const currentElement = text[i]
+			const currentLetter = text[i]
 			element.style.transition = "1s ease"
-			titelElement.appendChild(element)
+			headingElem.appendChild(element)
 			setTimeout(() => {
-				if (currentElement === " ") {
+				if (currentLetter === " ") {
 					element.innerHTML += " "
 				} else {
-					element.innerHTML += currentElement
+					element.innerHTML += currentLetter // avelacnel amen taric heto gcik, klaviaturayov grelu nman
 				}
 			}, i * 200)
 		}
 	}
 
-	const translateTitleToHeadingDiv = (titleElement) => {
+	const translateHeadingToHeaderSide = (headingElem) => {
 		setTimeout(() => {
-			titleElement.style.transform = "translateY(-100px)"
+			headingElem.style.transform = "translateY(-50px)"
 		}, 7000)
 	}
 
 	const createHeadingText = () => {
-		const headingText = "Hi, I'm Software Engineer !"
-		const title = document.createElement("div")
-		title.setAttribute("class", "title")
-		todoMiddle.appendChild(title)
-		let headingCurrentElement = document.createElement("h1")
-		headingCurrentElement.innerHTML = ""
-		addElementsInHeadingText(headingText, title, headingCurrentElement)
-		translateTitleToHeadingDiv(title)
+		const heading = document.createElement("div")
+		heading.setAttribute("class", "heading")
+		main.appendChild(heading)
+		const currentHeading = document.createElement("h1")
+		currentHeading.innerHTML = ""
+		addingLettersInHeadingText(p_data.headingText, heading, currentHeading)
+		translateHeadingToHeaderSide(heading)
 	}
 
 	createHeadingText()
 
 	setTimeout(() => {
-		photoDiv.style.opacity = "1"
-	}, 10000)
+		photoBlock.style.opacity = "1"
+	}, 8000)
 
 	setTimeout(() => {
-		nameElement.style.opacity = "1"
-	}, 13000)
+		name.style.opacity = "1"
+	}, 9000)
 
 	setTimeout(() => {
-		for (let i = 0; i < socialListElements.length; i++) {
+		for (let i = 0; i < socialListItems.length; i++) {
 			setTimeout(() => {
-				socialListElements[i].style.scale = "1.5"
-				socialListElements[i].style.opacity = "1"
-				socialListElements[i].style.transition = "0.2s ease"
-				if (socialListElements[i].style.opacity === "1") {
+				socialListItems[i].style.scale = "1.5"
+				socialListItems[i].style.opacity = "1"
+				socialListItems[i].style.transition = "0.2s ease"
+				if (socialListItems[i].style.opacity === "1") {
 					setTimeout(() => {
-						socialListElements[i].style.scale = "1"
+						socialListItems[i].style.scale = "1"
 					}, 200)
 				}
 			}, i * 500)
 		}
-	}, 15000)
+	}, 10000)
 
 	setTimeout(() => {
 		social.style.transform = "translateY(180px)"
-	}, 18000)
+	}, 11000)
 
 	setTimeout(() => {
-		photoDiv.style.transform = "translate(-490px)"
-		photoDiv.style.scale = "0.7"
-	}, 20000)
+		photoBlock.style.transform = "translate(-490px)"
+		photoBlock.style.scale = "0.7"
+	}, 12000)
 
 	setTimeout(() => {
-		introDiv.style.opacity = "1"
-	}, 22000)
+		introBlock.style.opacity = "1"
+	}, 13000)
 
 	setTimeout(() => {
-		introDiv.style.transform = "translate(490px)"
-		introDiv.style.scale = "0.7"
-	}, 24000)
+		introBlock.style.transform = "translate(490px)"
+		introBlock.style.scale = "0.7"
+	}, 14000)
 
 	setTimeout(() => {
-		portfolio.style.opacity = "1"
-	}, 28000)
+		portfolioBlock.style.opacity = "1"
+	}, 15000)
 
 	setTimeout(() => {
-		portfolio.style.transform = "translateY(200px)"
-		portfolio.style.scale = "1"
-	}, 30000)
+		portfolioBlock.style.transform = "translateY(200px)"
+		portfolioBlock.style.scale = "1"
+	}, 16000)
 
-	image.addEventListener("click", () => {
-		portfolio.style.transform = "translate(600px, 0px)"
-		portfolio.style.scale = "0.7"
-		introDiv.style.transform = "translate(-600px)"
-		introDiv.style.scale = "0.7"
-		photoDiv.style.transform = "translate(10px, 150px)"
-		photoDiv.style.scale = "1"
-	})
+	const clickEffects = (block1, block2, block3) => {
+		block1.style.transform = "translate(600px, 0px)"
+		block1.style.scale = "0.7"
+		block2.style.transform = "translate(-600px)"
+		block2.style.scale = "0.7"
+		block3.style.transform = "translate(10px, 150px)"
+		block3.style.scale = "1"
+	}
 
-	introDiv.addEventListener("click", () => {
-		photoDiv.style.transform = "translate(600px, 0px)"
-		photoDiv.style.scale = "0.7"
-		portfolio.style.transform = "translate(-600px)"
-		portfolio.style.scale = "0.7"
-		introDiv.style.transform = "translate(10px, 150px)"
-		introDiv.style.scale = "1"
-	})
+	image.addEventListener(
+		"click",
+		clickEffects(portfolioBlock, introBlock, photoBlock)
+	)
 
-	portfolio.addEventListener("click", () => {
-		introDiv.style.transform = "translate(600px, 0px)"
-		introDiv.style.scale = "0.7"
-		photoDiv.style.transform = "translate(-600px)"
-		photoDiv.style.scale = "0.7"
-		portfolio.style.transform = "translate(10px, 150px)"
-		portfolio.style.scale = "1"
-	})
+	introBlock.addEventListener(
+		"click",
+		clickEffects(photoBlock, portfolioBlock, introBlock)
+	)
+
+	portfolioBlock.addEventListener(
+		"click",
+		clickEffects(introBlock, portfolioBlock, photoBlock)
+	)
 }
 
-createPortfolioWebPage()
+createportfolioBlockWebPage()
